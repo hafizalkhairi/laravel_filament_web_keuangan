835b05a8c68eadfdbd2e8c54a336c36c222a61e3
unknown <hafizalkhairi88@gmail.com>
2024-12-03 14:42:34 +0700

    update filter data pada panel admin

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,19 +3,33 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
+use Carbon\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected function getStats(): array
     {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         $pemasukan = Transaction::join('categories', 'transactions.category_id', '=', 'categories.id')
             ->where('categories.is_expense', false)
+            ->whereBetween('transactions.transaction_date', [$startDate, $endDate])
             ->sum('transactions.amount');
 
         $pengeluaran = Transaction::join('categories', 'transactions.category_id', '=', 'categories.id')
             ->where('categories.is_expense', true)
+            ->whereBetween('transactions.transaction_date', [$startDate, $endDate])
             ->sum('transactions.amount');
         return [
             Stat::make('Total Pemasukan', 'Rp ' . number_format($pemasukan, 0, ',', '.')),
--- app/Filament/Widgets/WidgetIncomeChart.php
+++ app/Filament/Widgets/WidgetIncomeChart.php
@@ -4,21 +4,34 @@ namespace App\Filament\Widgets;
 
 use App\Models\Transaction;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Carbon;
 
 class WidgetIncomeChart extends ChartWidget
 {
+
+    use InteractsWithPageFilters;
+
     protected static ?string $heading = 'Pemasukan';
     protected static string $color = 'success';
     protected static ?string $pollingInterval = '10s';
 
     protected function getData(): array
     {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+        Carbon::parse($this->filters['startDate']) :
+        null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         $data = Trend::query(Transaction::income())
         ->between(
-            start: now()->startOfYear(),
-            end: now()->endOfYear(),
+            start: $startDate,
+            end: $endDate,
         )
         ->perDay()
         ->count();
